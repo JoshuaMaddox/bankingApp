fcce32f48cc5ba9faf6903a561f33dfcef542e30
Joshua Maddox <HomeFolder@Joshuas-MacBook-Pro.local>
2016-09-17 12:34:40 -0700

    fixed delete button - starting on edit feature

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const BankingApp = React.createClass({
-
   getInitialState() {
     return({
       transactions: [],
@@ -10,18 +9,7 @@ const BankingApp = React.createClass({
       balance: 0
     })
   },
-
-  // // TYRING to get balance
-  // updateBalance(transaction) {
-    
-  //   // let newBalance = this.debitsTotal
-  //   // newBalance += this.transaction.debit
-  //   // this.setState({
-  //   //   this.debitsTotal += 
-  //   // })
-  // },
   
-
   //Get the transaction amount and add or subtract it from the balance
   _addTransaction(transaction) {
     //Destructure the state objects
@@ -37,52 +25,67 @@ const BankingApp = React.createClass({
     })
   },
 
+  _removeTransaction(transactionID) {
+    console.log('I am the transaction ID in _removeTransaction: ', transactionID)
+    //Destructure the transactions object, gaining access to the
+    //individual transactions
+    const { transactions } = this.state
+    this.setState({
+      transactions: transactions.filter(transaction => transaction.transactionID)
+    })
+  },
+
   updateTotal(creditsTotal, debitsTotal) {
     console.log('creditsTotal: ', creditsTotal, '\ndebitsTotal: ', debitsTotal)
   },
 
   //Add a timestap to the transaction
   render() {
+    const { transactions } = this.state
+
     return(
-      <div>
         <div className="container">
-          <h3 className="debits">
-            <span id='debitsTotal' className="balances">Total Debits: {this.state.debitsTotal}</span>
-            <span id='creditsTotal' className="balances">Total Credits: {this.state.creditsTotal}</span>
-            <span id='balance' className="balances">Balance: {this.state.balance}</span>
-          </h3>
-        </div>
-        <NewTransactionForm  _addTransaction={this._addTransaction} updateTotal={this.updateTotal} />
-      </div>
+          <div className="row">
+            <NewTransactionForm  _addTransaction={this._addTransaction} updateTotal={this.updateTotal} />
+              <h3 className="debits">
+                <span id='debitsTotal' className="balances">Total Debits: {this.state.debitsTotal}</span>
+                <span id='creditsTotal' className="balances">Total Credits: {this.state.creditsTotal}</span>
+                <span id='balance' className="balances">Balance: {this.state.balance}</span>
+              </h3> 
+            <TransactionsTable transactions={transactions} _removeTransaction={this._removeTransaction} />
+          </div>
+        </div> 
     )  
   }
 })
 
-//_____________________________________________________________________________________________________________________________
 
-
-const ProductTable = props => {
-  const { products, removeProduct } = props;
+const TransactionsTable = props => {
+  //Destructure the transactions object that is 
+  //wired via <TransactionsTable transactions={transactions} />
+  const { transactions, _removeTransaction } = props;
 
   return (
-    <table className="table">
+    <table className="table ">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Delete</th>
+          <th>DESCRIPTION</th>
+          <th>AMOUNT</th>
+          <th>TYPE</th>
+          <th>DATE</th>
         </tr>
       </thead>
       <tbody>
         {/*Video 22:59*/}
-        {products.map(product => {
+        {transactions.map(transaction => {
           //could just wrap with parenthesis
           return (
-            <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.price}</td>
+            <tr key={transaction.transactionID}>
+              <td>{transaction.description}</td>
+              <td>{transaction.debit + transaction.credit}</td>
+              <td>{transaction.transactionType}</td>
               <td>
-                <button onClick={removeProduct.bind(null, product.id)} className="btn btn-danger">x</button>
+                <button onClick={_removeTransaction.bind(null, transaction.transactionID)} className="btn btn-danger">x</button>
               </td>
             </tr>
           )
@@ -92,10 +95,7 @@ const ProductTable = props => {
   )
 }
 
-//==============================================================================================================================
-
 const NewTransactionForm = React.createClass({
-
   submitForm(event) {
     event.preventDefault()
 
@@ -141,27 +141,27 @@ const NewTransactionForm = React.createClass({
     return(
       <div className="container">
         <div className="row">
-          <h1>Banking Stuff</h1>
-          
-        </div>{/* ROW */}
-        <div className="form-container">
-          <form onSubmit={this.submitForm}>
-            <div className="form-group">
-              <label htmlFor="newTransaction">Transaction Description: </label>
-              <input ref='description' type="text" className='form-control' id='newTrans' required/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="newPrice">Transaction Amount:</label>
-              <input ref='amount' type="number" className='form-control' id='newAmount' min='0' step='0.01' required/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="transactionType">Is this transaction a Debit or Credit?</label>
-              <input ref='transactionType' type="text" className='form-control' id='transactionType' required/>
-            </div>
-            <button   id="debit" value='debit'  className='btn btn-danger' ref='debit'>Submit Transaction</button>
-          </form>
-            {/*<button  onClick={() => updateTotal(1,2)} >Test</button>*/}
-        {/* FORM-CONTAINER */}
+          <h1>Banking Stuff</h1> 
+          <div className="form-container">
+            <form onSubmit={this.submitForm}>
+              <div className="form-group">
+                <label htmlFor="newTransaction">Transaction Description: </label>
+                <input ref='description' type="text" className='form-control' id='newTrans' required/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="newPrice">Transaction Amount:</label>
+                <input ref='amount' type="number" className='form-control' id='newAmount' min='0' step='0.01' required/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="transactionType">Is this transaction a Debit or Credit?</label>
+                <input ref='transactionType' type="text" className='form-control' id='transactionType' required/>
+              </div>
+              <button   id="debit" value='debit'  className='btn btn-danger' ref='debit'>Submit Transaction</button>
+            </form>
+              {/*<button  onClick={() => updateTotal(1,2)} >Test</button>*/}
+          {/* FORM-CONTAINER */}
+          </div>
+        {/* ROW */}
         </div>
       {/* CONTAINER */}
       </div>
